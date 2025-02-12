fb9535acdc3325dcb6b2b45337160f9f36f2fcac
saksham021012 <sakshamgoyal2k4@gmail.com>
2025-02-13 01:01:06 +0530

    fix: update user controller to use ApiResponse and await for user updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -254,7 +254,7 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
 const getCurrentUser = asyncHandler(async(req,res)=>{
     return res
     .status(200)
-    .json(200, req.user, "Current user fetched successfully")
+    .json(new ApiResponse(200, req.user, "Current user fetched successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async(req,res)=>{
@@ -264,7 +264,7 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
         throw new ApiError(400, "All fields are required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
@@ -326,7 +326,7 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
 
     const user = await User.findByIdAndUpdate(
         req.user?._id,
-        {
+        { n
             $set:{
                 coverImage: coverImage.url
             }
